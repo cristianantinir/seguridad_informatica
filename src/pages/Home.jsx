8b90732b6d3ecaa7a18a60acd4728a771689cf9b
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-28 20:14:39 -0300

    Agregando imports y estructura inicial de la página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import style from './Home.module.css';
+import Footer from '../components/Footer';
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default Home;
