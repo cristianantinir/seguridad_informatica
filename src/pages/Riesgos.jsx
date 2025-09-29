18bbe69437299c713e668b50962ff89a9dc4d788
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-29 14:11:43 -0300

    Imports necesarios + función de la página 'Riesgos'

---
+++ src/pages/Riesgos.jsx
@@ -0,0 +1,13 @@
+import {useNavigate } from "react-router-dom";
+import style from './Riesgos.module.css';
+import Footer from '../components/Footer';
+
+function Riesgos() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+    </>
+  );
+}
+export default Riesgos;
