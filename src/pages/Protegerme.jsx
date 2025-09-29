edc3d8e9ee2b12b0c4ac27cfb7019a3d662b9038
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-29 13:54:14 -0300

    Imports necesarios + función a la página 'Protegerme'

---
+++ src/pages/Protegerme.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import style from "./Protegerme.module.css"
+import Footer from '../components/Footer';
+
+function Protegerme (){
+    const navigate = useNavigate();
+
+    return(
+       <>
+        </>
+    );
+}
+export default Protegerme;
