f97010152b216369b1176b9844e1548bf11c7dcc
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-29 13:55:55 -0300

    Contenido + footer a la página 'Protegerme'

--- src/pages/Protegerme.jsx
+++ src/pages/Protegerme.jsx
@@ -7,6 +7,19 @@ function Protegerme (){
 
     return(
        <>
+        <div className={style.contenedorProtegerme}>
+            <div className={style.conPro}>
+                <h1>¿Cómo Protegerme?✅</h1>
+                <button onClick={() => navigate('/')}>Volver</button>
+            </div>
+            <div className={style.contenidoRiesgo}>
+                <p>La mejor defensa contra el malware es la prevención. Mantén siempre tu sistema operativo, navegadores y aplicaciones actualizados. Muchas amenazas se aprovechan de errores ya corregidos, por eso es clave instalar las actualizaciones de seguridad.
+                    Usa un antivirus confiable y evita hacer clic en enlaces sospechosos o abrir archivos de remitentes desconocidos. Desconfía de correos, mensajes o ventanas emergentes que te pidan datos personales o contraseñas.
+                    Además, haz copias de seguridad regularmente y utiliza contraseñas seguras y únicas para cada cuenta. Con hábitos simples, puedes reducir drásticamente el riesgo de infección.
+                </p>
+            </div>
+            <Footer /> 
+        </div>
         </>
     );
 }
