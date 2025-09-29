61fedc287b420f5d257fd16484c464408e65aa45
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-29 14:12:39 -0300

    Cargando contenido a la página 'Riesgos'

--- src/pages/Riesgos.jsx
+++ src/pages/Riesgos.jsx
@@ -7,6 +7,19 @@ function Riesgos() {
 
   return (
     <>
+    <div className={style.contenedorRiesgo}>
+        <div className={style.cajon}>
+            <h1>Riesgos de malware⚠️</h1>
+            <button onClick={() => navigate('/')}>Volver</button>  
+        </div>
+        <div className={style.contenido}>
+            <p>El malware, en sus distintas formas, representa un riesgo constante para usuarios y organizaciones. Su capacidad para infiltrarse sin ser detectado lo convierte en una amenaza difícil de prevenir si no se toman las medidas adecuadas.
+                Los efectos pueden ir desde una simple molestia hasta consecuencias graves como el robo de datos sensibles, el secuestro de archivos, la pérdida de dinero o el control remoto del dispositivo afectado.
+                Además, muchos tipos de malware se propagan fácilmente en redes, lo que amplifica el daño a otros sistemas conectados. Estar informado y protegido es clave para reducir este riesgo.
+            </p>
+        </div>
+        <Footer /> 
+    </div>
     </>
   );
 }
