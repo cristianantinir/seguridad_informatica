d50629d1dc6020edad974591ce76476dce26897a
Cristian Antinir <cristian.antinir.tech@gmail.com>
2025-09-29 14:27:20 -0300

    Creando footer + contenido

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import style from './Footer.module.css';
+
+function Footer() {
+
+  return (
+    <>
+    <footer>
+      Proyecto final desarrollado por Cristian Antiñir — EPET N°20, Neuquén, Argentina. <br />
+      Contacto: cristian.antinir.tech@gmail.com
+    </footer>
+    </>
+  );
+}
+
+export default Footer;
